75548b83aab502190aa634be33b8021c78fe8266
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2023-07-12 17:25:55 +0530

    created UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [person, setPerson] = useState({
+    name: "",
+    email: "",
+    branch: "",
+    phnNum: "",
+  })
+
+  function validate() {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="login-box">
+        <h2>Registration Form</h2>
+        <form name='myForm' onSubmit={validate()}>
+          <div class="user-box">
+            <input type="text" name="name" required />
+            <label>Name</label>
+          </div>
+          <div class="user-box">
+            <input type="email" name="email" required />
+            <label>Email</label>
+          </div>
+          <div class="user-box">
+            <input type="text" name="branch" required />
+            <label>Branch</label>
+          </div>
+          <div class="user-box">
+            <input type="text" name="phnNum" required />
+            <label>Phone Number</label>
+          </div>
+          <button type='submit'>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }

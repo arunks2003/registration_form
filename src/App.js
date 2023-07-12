1b7a1bd95ee9326afcd7f6a52d1fd798285274ce
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2023-07-12 21:49:39 +0530

    Validation done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,91 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [person, setPerson] = useState({
+  const initialValues = {
     name: "",
     email: "",
     branch: "",
     phnNum: "",
-  })
+  }
 
-  function validate() {
+  const [person, setPerson] = useState(
+    // name: "",
+    // email: "",
+    // branch: "",
+    // phnNum: "",
+    initialValues
+  )
+  const [errors, setErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);//just a flag for check submit
 
+  function handleChange(event) {
+    // console.log(event.target)
+    const { name, value } = event.target;
+    setPerson({ ...person, [name]: value });//now name takes as a key and value is assigned to a key
+    console.log(person);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setErrors(validate(person));
+    setIsSubmit(true);
+  }
+
+  useEffect(() => {
+    console.log(errors);
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log(person);
+    }
+  }, [errors]);
+
+  function validate(values) {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.name) {
+      errors.name = "*Name is required!";
+    }
+    if (!values.email) {
+      errors.email = "*Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "*Email is not in a valid email format!";
+    }
+    if (!values.branch) {
+      errors.branch = "*Branch is required";
+    }
+    if (!values.phnNum) {
+      errors.phnNum = "*Phone Number is required";
+    } else if (values.phnNum.length < 10) {
+      errors.phnNum = "*Phone Number must be atleast 10 integers";
+    }
+    return errors;
   }
   return (
     <div className="App">
       <div class="login-box">
         <h2>Registration Form</h2>
-        <form name='myForm' onSubmit={validate()}>
+        {/* <pre>{JSON.stringify(person, undefined, 2)}</pre> */}
+        <form name='myForm' onSubmit={handleSubmit} autoComplete='off'>
           <div class="user-box">
-            <input type="text" name="name" required />
+            <input type="text" name="name" required autoComplete='off' value={person.name} onChange={handleChange} />
             <label>Name</label>
+            <p className='errormsg'>{errors.name}</p>
           </div>
           <div class="user-box">
-            <input type="email" name="email" required />
+            <input type="text" name="email" required autoComplete='off' value={person.email} onChange={handleChange} />
             <label>Email</label>
+            <p className='errormsg'>{errors.email}</p>
           </div>
           <div class="user-box">
-            <input type="text" name="branch" required />
+            <input type="text" name="branch" required autoComplete='off' value={person.branch} onChange={handleChange} />
             <label>Branch</label>
+            <p className='errormsg'>{errors.branch}</p>
           </div>
           <div class="user-box">
-            <input type="text" name="phnNum" required />
+            <input type="text" name="phnNum" required autoComplete='off' value={person.phnNum} onChange={handleChange} />
             <label>Phone Number</label>
+            <p className='errormsg'>{errors.phnNum}</p>
           </div>
           <button type='submit'>
             <span></span>
